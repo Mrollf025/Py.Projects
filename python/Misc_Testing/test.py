e70219a81e8d45dce3e523aacdee2a3f45c7c332
Mrollf025 <107584334+Mrollf025@users.noreply.github.com>
2024-12-07 06:35:28 -0500

    I forget what I was doing
    I forget what I was doing

---
+++ python/Misc_Testing/test.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+path = Path("python\Misc_Testing\list_of_projects.txt")
+text = path.read_text()
+
--- python/test.py
+++
@@ -1,6 +0,0 @@
-my_list = [10, 20, 30, 40, 50]
-
-# Iterate over the list with the index using enumerate
-for index, value in enumerate(my_list):
-    if index == 0:
-        print(f"The value at index 0 is: {value}")
